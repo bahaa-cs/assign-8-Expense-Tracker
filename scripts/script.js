5bb4d5d811eabbdabf949ae5bd5883eea5f18b4c
bahaaCs <bahaaaboukhalil@gmail.com>
2024-11-03 18:36:44 +0200

    Added logic to list transaction created

--- scripts/script.js
+++ scripts/script.js
@@ -19,6 +19,26 @@ document.getElementById("transactionForm").addEventListener("submit", (event) =>
     event.preventDefault()
     
     saveTransaction()
+
+    let transaction_info_element=document.getElementById("transaction-info")
+    
+    let transactions =JSON.parse(localStorage.getItem("Transactions"))
+
+    let length=transactions.length
+    transaction_info_element.innerHTML+=
+    
+    `<div class="flex row data black-txt" id="${length}">
+        <div class="price">${transactions[length-1].price}</div>
+        <div class="type">${transactions[length-1].type}</div>
+        <div class="date">${transactions[length-1].date}</div>
+        <div class="notes">${transactions[length-1].notes}</div>
+        <div class="flex row action-btns black-txt">
+            <button class="edit-btn">edit</button>
+            <button class="delete-btn">delete</button>
+        </div>
+    </div>`
+
+
     console.log(localStorage.getItem("Transactions"))
 })
 
