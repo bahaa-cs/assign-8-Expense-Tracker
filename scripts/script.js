987afa75cb59281e56730db402176260c874e14c
bahaaCs <bahaaaboukhalil@gmail.com>
2024-11-03 15:38:05 +0200

    Created function: Storing input successfully into local storage

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,23 @@
-let transactions = {
-    price:"0",
-    type: "income",
-    date: "2024",
-    notes: "test",
+localStorage.clear()
+const saveTransaction = () => {
+    let transactions = JSON.parse(localStorage.getItem("Transactions")) || []
+
+    const newTransaction = {
+        price: document.getElementById("price").value,
+        type: document.getElementById("type").value,
+        date: document.getElementById("date").value,
+        notes: document.getElementById("notes").value,
+    }
+
+    transactions.push(newTransaction)
+
+    localStorage.setItem("Transactions", JSON.stringify(transactions))
+    document.getElementById("transactionForm").reset()
 }
 
-localStorage.setItem("Transactions",JSON.stringify(transactions))
+document.getElementById("transactionForm").addEventListener("submit", (event) => {
+    event.preventDefault()
+    saveTransaction()
+    console.log(localStorage.getItem("Transactions"))
+})
 
-console.log(localStorage.getItem("Transactions"))
