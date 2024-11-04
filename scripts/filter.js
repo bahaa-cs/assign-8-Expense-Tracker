a977e4062409a826dd56bb4011e75776900acb95
bahaaCs <bahaaaboukhalil@gmail.com>
2024-11-04 21:31:09 +0200

    Added logic to filter all at once

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,9 +1,13 @@
 document.getElementById("filter").addEventListener("click", () => {
-    filter_minPrice()
-    filter_maxPrice()
-    filter_type()
-    filter_date()
-    filter_notes()
+    // to filter all at once
+    minPrice_transaction = filter_minPrice(transactions)
+    max_price_transaction = filter_maxPrice(minPrice_transaction)
+    type_transaction = filter_type(max_price_transaction)
+    date_transaction = filter_date(type_transaction)
+    
+    final_transaction = filter_notes(date_transaction)
+
+    reloadLocalStorage(final_transaction)
 
 })
 const filter_minPrice = () =>{
@@ -14,10 +18,11 @@ const filter_minPrice = () =>{
     })
     localStorage.setItem("newTransactions", JSON.stringify(newTransactions))
     if (minPrice!==0)
-    reloadLocalStorage(newTransactions)
+        return newTransactions
+    return transactions
 }
 
-const filter_maxPrice = () =>{
+const filter_maxPrice = (transactions) =>{
     let maxPrice = Number(document.getElementById("max-price-filter").value)
     
     let newTransactions = transactions.filter(transaction => {
@@ -25,21 +30,23 @@ const filter_maxPrice = () =>{
     })
     localStorage.setItem("newTransactions", JSON.stringify(newTransactions))
     if(maxPrice!==0)
-    reloadLocalStorage(newTransactions)
+        return newTransactions
+    return transactions
 };
 
-const filter_type = () =>{
+const filter_type = (transactions) =>{
     let type = document.getElementById("type-filter").value
     let newTransactions = transactions.filter(transaction => {
         return transaction.type == type
     })
     localStorage.setItem("newTransactions", JSON.stringify(newTransactions))
     if(type=="income" || type=="expense")
-    reloadLocalStorage(newTransactions)
+        return newTransactions
+    return transactions
 
 }
 
-const filter_date = () =>{
+const filter_date = (transactions) =>{
     let date = document.getElementById("date-filter").value
 
     let newTransactions = transactions.filter(transaction => {
@@ -47,18 +54,20 @@ const filter_date = () =>{
     })
     localStorage.setItem("newTransactions", JSON.stringify(newTransactions))
     if(date)
-    reloadLocalStorage(newTransactions)
+        return newTransactions
+    return transactions
 
 }
 
-const filter_notes = () =>{
+const filter_notes = (transactions) =>{
     let notes = document.getElementById("notes-filter").value
 
     let newTransactions = transactions.filter(transaction => {
         return transaction.notes.includes(notes)  })
     localStorage.setItem("newTransactions", JSON.stringify(newTransactions))
     if(notes)
-    reloadLocalStorage(newTransactions)
+        return newTransactions
+    return transactions
 
 }
 
