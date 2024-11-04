03fc8131c880ca02a20803ef5c5af85da977415a
bahaaCs <bahaaaboukhalil@gmail.com>
2024-11-04 20:52:39 +0200

    filter on max price

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,11 +1,28 @@
 document.getElementById("filter").addEventListener("click", () => {
+    filter_minPrice()
+    filter_maxPrice()
+
+})
+const filter_minPrice = () =>{
     let minPrice = Number(document.getElementById("min-price-filter").value)
     
     let newTransactions = transactions.filter(transaction => {
        return Number(transaction.price) >= minPrice
     })
     localStorage.setItem("newTransactions", JSON.stringify(newTransactions))
+    if (minPrice!==0)
     reloadLocalStorage(newTransactions)
-    console.log(transactions)
-    console.log(newTransactions)
-});
+}
+
+const filter_maxPrice = () =>{
+    let maxPrice = Number(document.getElementById("max-price-filter").value)
+    
+    let newTransactions = transactions.filter(transaction => {
+       return Number(transaction.price) <= maxPrice
+    })
+    localStorage.setItem("newTransactions", JSON.stringify(newTransactions))
+    if(maxPrice!==0)
+    reloadLocalStorage(newTransactions)
+};
+
+
